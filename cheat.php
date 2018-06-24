cd70dc0f44d43f5e67ee2f9c31aae5ebad19400c
hintswen <hintswen@users.noreply.github.com>
2018-06-24 15:07:11 +1000

    one more space

--- cheat.php
+++ cheat.php
@@ -306,7 +306,7 @@ function GetFirstAvailableZone( $Planet, &$ZonePaces )
 			if ( $PaceCutoff > 0.02 )
 			{
 				global $WaitTime;
-				$Time = ceil( ( 1 - $Zone[ 'capture_progress' ]) / $PaceCutoff * $WaitTime );
+				$Time = ceil( ( 1 - $Zone[ 'capture_progress' ] ) / $PaceCutoff * $WaitTime );
 				$Minutes = floor( $Time / 60 );
 				$Seconds = $Time % 60;
 				Msg( '-- Current pace for Zone {green}' . $Zone[ 'zone_position' ] . '{normal} is {green}+' . number_format( $PaceCutoff * 100, 2 ) . '%{normal} ETA: {green}' . $Minutes . 'm ' . $Seconds . 's' );
