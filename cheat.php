ee6210d0f3621e446456eba927cfd9b121a33a75
hintswen <hintswen@users.noreply.github.com>
2018-06-24 15:08:17 +1000

    PHP: Added ETA to zone pacing (#86)
    * When only easy planets remain, refresh every 10 minutes
    
    Fix for Issue #75
    
    * PHP: Added ETA to zone pacing
    
    * Revert "When only easy planets remain, refresh every 10 minutes"
    
    This reverts commit ee687e9eaeed861c72005091f9756a0c5d985874.
    
    * fixed formatting and switched to WaitTiem variable
    
    * one more space

--- cheat.php
+++ cheat.php
@@ -305,7 +305,11 @@ function GetFirstAvailableZone( $Planet, &$ZonePaces )
 
 			if ( $PaceCutoff > 0.02 )
 			{
-				Msg( '-- Current pace for Zone {green}' . $Zone[ 'zone_position' ] . '{normal} is {green}+' . number_format( $PaceCutoff * 100, 2 ) . '%' );
+				global $WaitTime;
+				$Time = ceil( ( 1 - $Zone[ 'capture_progress' ] ) / $PaceCutoff * $WaitTime );
+				$Minutes = floor( $Time / 60 );
+				$Seconds = $Time % 60;
+				Msg( '-- Current pace for Zone {green}' . $Zone[ 'zone_position' ] . '{normal} is {green}+' . number_format( $PaceCutoff * 100, 2 ) . '%{normal} ETA: {green}' . $Minutes . 'm ' . $Seconds . 's' );
 			}
 
 			$PaceCutoff = 0.98 - $PaceCutoff;
