dddb57c5eb34930948024bc1890745a780067c78
Pybro <36242149+Sparkx8@users.noreply.github.com>
2018-06-25 21:00:27 +0200

    Add basic instructions if powershell version too low and alternative decompress method (#159)

--- downloadphp.ps1
+++ downloadphp.ps1
@@ -1,5 +1,31 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://windows.php.net/downloads/releases/php-7.2.7-nts-Win32-VC15-x64.zip -OutFile php.zip
-Expand-Archive -LiteralPath php.zip -DestinationPath php\
+$workingdir = (Get-Location).Path
+if($PSVersiontable.PSVersion.Major -lt 3)
+{
+    Write-Warning "Please download php and place it in the same folder as this script."
+    Write-Output "Download from: https://windows.php.net/downloads/releases/php-7.2.7-nts-Win32-VC15-x64.zip"
+    Write-Output ("Save to this directory: $workingdir `nand rename it to php.zip" -f (Get-Location).Path)
+    Read-Host "Press Enter when you're done!"
+}
+else
+{
+    Invoke-WebRequest -Uri https://windows.php.net/downloads/releases/php-7.2.7-nts-Win32-VC15-x64.zip -OutFile php.zip
+}
+if($PSVersiontable.PSVersion.Major -lt 5)
+{
+    if((Test-Path "$workdingdir\php") -eq $False)
+    {
+        Add-Type -AssemblyName System.IO.Compression.FileSystem
+        [IO.Compression.ZipFile]::ExtractToDirectory('php.zip', 'php\')
+    }
+    else
+    {
+        Write-Warning "Directory $work\php already found. Delete if necessary!"
+    }
+}
+else
+{
+    Expand-Archive -LiteralPath php.zip -DestinationPath php\ -Force
+}
 Copy-Item -Path php\php.ini-production -Destination php\php.ini
 ((Get-Content php\php.ini)) -Replace ";extension=curl", ("extension=" + (Get-Item -Path ".\php") + "\ext\php_curl.dll") | Set-Content php\php.ini
