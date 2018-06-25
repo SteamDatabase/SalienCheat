a61d3e5240ad1e6a66aeeaa22724f2cbf6fb4811
BrenenP <xdooncex@gmail.com>
2018-06-25 09:32:05 -0400

    Windows only

--- cheat.py
+++ cheat.py
@@ -18,8 +18,9 @@ from tqdm import tqdm
 
 import ctypes
 
-kernel32 = ctypes.windll.kernel32
-kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+if sys.platform == "win32":
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 try:
     _input = raw_input
