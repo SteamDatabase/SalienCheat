aa7c3d81f4cfb524e87bee0773bd3b6f4e9c922f
Alamei <Alamei@users.noreply.github.com>
2018-06-25 21:51:47 -0600

    Remove extraneous parameters and variables related to cutoff functionality.

--- cheat.php
+++ cheat.php
@@ -82,7 +82,7 @@ while( !isset( $Data[ 'response' ][ 'score' ] ) );
 
 do
 {
-	$BestPlanetAndZone = GetBestPlanetAndZone( $SkippedPlanets, $KnownPlanets, $WaitTime );
+	$BestPlanetAndZone = GetBestPlanetAndZone( $SkippedPlanets, $KnownPlanets );
 }
 while( !$BestPlanetAndZone && sleep( 5 ) === 0 );
 
@@ -116,7 +116,7 @@ do
 
 		do
 		{
-			$BestPlanetAndZone = GetBestPlanetAndZone( $SkippedPlanets, $KnownPlanets, $WaitTime );
+			$BestPlanetAndZone = GetBestPlanetAndZone( $SkippedPlanets, $KnownPlanets );
 		}
 		while( !$BestPlanetAndZone && sleep( 5 ) === 0 );
 
@@ -158,7 +158,7 @@ do
 
 	do
 	{
-		$BestPlanetAndZone = GetBestPlanetAndZone( $SkippedPlanets, $KnownPlanets, $WaitTime );
+		$BestPlanetAndZone = GetBestPlanetAndZone( $SkippedPlanets, $KnownPlanets );
 	}
 	while( !$BestPlanetAndZone && sleep( 5 ) === 0 );
 
@@ -275,7 +275,7 @@ function GetNameForDifficulty( $Zone )
 	return $Boss . $Difficulty;
 }
 
-function GetPlanetState( $Planet, $WaitTime )
+function GetPlanetState( $Planet )
 {
 	$Zones = SendGET( 'ITerritoryControlMinigameService/GetPlanet', 'id=' . $Planet . '&language=english' );
 
@@ -328,7 +328,6 @@ function GetPlanetState( $Planet, $WaitTime )
 			case 1: $LowZones++; break;
 		}
 
-		$Zone[ 'cutoff' ] = $Cutoff;
 		$CleanZones[] = $Zone;
 	}
 
@@ -358,7 +357,7 @@ function GetPlanetState( $Planet, $WaitTime )
 	];
 }
 
-function GetBestPlanetAndZone( &$SkippedPlanets, &$KnownPlanets, $WaitTime )
+function GetBestPlanetAndZone( &$SkippedPlanets, &$KnownPlanets )
 {
 	$Planets = SendGET( 'ITerritoryControlMinigameService/GetPlanets', 'active_only=1&language=english' );
 
@@ -385,7 +384,7 @@ function GetBestPlanetAndZone( &$SkippedPlanets, &$KnownPlanets, $WaitTime )
 
 		do
 		{
-			$Zone = GetPlanetState( $Planet[ 'id' ], $WaitTime );
+			$Zone = GetPlanetState( $Planet[ 'id' ] );
 		}
 		while( $Zone === null && sleep( 5 ) === 0 );
 
