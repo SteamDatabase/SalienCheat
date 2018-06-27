2557df529f266720f1da0b3e85b942ac6f5b0647
Pavel Djundik <github@xpaw.me>
2018-06-27 18:09:50 +0300

    Use round instead of ceil

--- cheat.php
+++ cheat.php
@@ -207,7 +207,7 @@ do
 
 	if( $Data[ 'eresult' ] == 93 )
 	{
-		$LagAdjustedWaitTime = min( 10, ceil( $SkippedLagTime + 0.3 ) );
+		$LagAdjustedWaitTime = min( 10, round( $SkippedLagTime ) );
 
 		Msg( '{lightred}-- Time is out of sync, trying again in ' . $LagAdjustedWaitTime . ' seconds...' );
 
